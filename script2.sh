d5ce15abb4840f312223d0afd8b62fd85a0d6558
Luis3nov <luis3novillo@gmail.com>
2025-12-03 15:52:46 +0000

    tercero

---
+++ script2.sh
@@ -0,0 +1,4 @@
+uname -a #Muestra información del sistema operativo y del kernel. (uname) muestra información del sistema y (-a) muestra TODA la información disponible
+lscpu #Muestra información detallada del procesador.
+free -h #Muestra uso de memoria RAM y swap. (free) muestra la memoria libre y usada (-h) significa Human-readable (formato legible para humanos: KB, MB, GB)
+df -h #Muestra uso del disco duro o espacio en disco. (df) significa Disk Free (espacio libre en disco). Y otra vez (-h) para que sea un formato legible para el humano.
