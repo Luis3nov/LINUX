a35a65ba0547ed205bae62e1f06a739a4a4ae504
Luis3nov <luis3novillo@gmail.com>
2025-12-03 15:41:44 +0000

    mi primer script

---
+++ script.sh
@@ -0,0 +1,6 @@
+#Crear y listar archivos
+echo "Hola Linux" > archivo.txt #el comando echo imprime texto en la terminal y el operador > de redirección de salida, toma la salida de echo y la guarda en un archivo.
+cat archivo.txt #comando cat muestra el contenido de un archivo en la terminal.
+cp archivo.txt copia.txt #comando cp copia archivos o directorios. Crea una copia exacta de archivo.txt llamada copia.txt.
+ls -l #comando ls Lista los archivos y directorios.
+rm archivo.txt
