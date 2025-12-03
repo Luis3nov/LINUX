2488ec6f4c209ccaa34e3d84a073cbce293364ca
Luis3nov <luis3novillo@gmail.com>
2025-12-03 15:45:09 +0000

    segundo

--- script.sh
+++ script.sh
@@ -2,5 +2,5 @@
 echo "Hola Linux" > archivo.txt #el comando echo imprime texto en la terminal y el operador > de redirección de salida, toma la salida de echo y la guarda en un archivo.
 cat archivo.txt #comando cat muestra el contenido de un archivo en la terminal.
 cp archivo.txt copia.txt #comando cp copia archivos o directorios. Crea una copia exacta de archivo.txt llamada copia.txt.
-ls -l #comando ls Lista los archivos y directorios.
-rm archivo.txt
+ls -l #comando ls Lista los archivos y directorios.y -l Muestra formato largo con los detalles.
+rm archivo.txt #comando rm Elimina directamente archivos o directorios.
